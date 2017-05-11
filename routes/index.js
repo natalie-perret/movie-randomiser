aec83e67601bc9bec3bd7f32313519983e08c488
eda013 <poukupenga@gmail.com>
2017-05-12 10:37:41 +1200

    routes written - no db yet

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,16 @@ var router = express.Router()
 var db = require('../db')
 
 router.get('/', function (req, res) {
-  res.send('sup')
+  res.render('home')
+})
+
+router.get('/addmovie', function (req, res) {
+  res.render('addmovie')
+})
+
+router.get('/movie/:id', function (req, res) {
+  db.getMovie(req.params.id, req.app.get('connection'))
+    .then((result) => res.render('movie', result))
 })
 
 module.exports = router
