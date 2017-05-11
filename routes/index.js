dc493c785abd9b6332fbf2ded129c41779409708
simonol1 <simondanolearyis@gmail.com>
2017-05-12 11:28:52 +1200

    corrected bodyparser in server.js

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,7 @@ var router = express.Router()
 var db = require('../db')
 
 router.get('/', function (req, res) {
-  db.getUsers(req.app.get('connection'))
-    .then(function (users) {
-      res.render('index', { users: users })
-    })
-    .catch(function (err) {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
+  res.send('hello')
 })
 
 module.exports = router
