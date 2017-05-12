760e8af3435776cc1b17ddf6202d301e51139a45
Thomasin Abraham <thomasinabraham@me.com>
2017-05-12 14:40:41 +1200

    hello

--- db.js
+++ db.js
@@ -1,24 +1,89 @@
 
+var movieDb = require('moviedb')('5d6500fd40637e64552fe0ce86c7e9ca')
 
+// function allGenres (connection) {
+//   return connection('genres').select(name)
+// }
+//
+// function getMovie (id, connection) {
+//   return connection('movies').select()
+//     .where('id', id)
+//     .first()
+// }
+//
+// function getMovieGenres (movieId, connection) {
+//   return connection('genres')
+//   .join()
+// }
 
-function allGenres (connection) {
-  return connection('genres').select(name)
+// Api related stuff ->
+
+function addRandom() {
+  getLatestMovie()
+    .then(findById)
+    .then((result) => {
+      // when 'Add to database' function created can substitute that instead of console log
+      console.log(`${result.original_title}, ${result.release_date.slice(0,4) || "No release date )-:"}
+${result.overview || "No description )-:"}`)
+    })
+    .catch(console.log)
 }
 
-function getMovie (id, connection) {
-  return connection('movies').select()
-    .where('id', id)
-    .first()
+function getLatestMovie() {
+  return new Promise((resolve, reject) => {
+    movieDb.miscLatestMovies((err, res) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(res)
+    })
+  })
 }
 
-function getMovieGenres (movieId, connection) {
-  return connection('genres')
-  .join()
+function findById(result) {
+  let randId = Math.floor(Math.random() * result.id) + 1
+  return new Promise((resolve, reject) => {
+    movieDb.movieInfo({id: randId}, (err, res) => {
+      if (err) {
+        if (err.response.body.status_code != 34) { reject(err) }
+        resolve(findById(result))
+      }
+      resolve(res)
+    })
+  })
+}
+
+function searchMovies(keyword) {
+  return new Promise((resolve, reject) => {
+    movieDb.searchMovie({query: keyword}, (err, result) => {
+      if (err) {
+        console.log(err)
+        reject(err)
+      } else {
+        resolve(result)
+      }
+  })
+})
+}
+
+function addTitle(movie_id) {
+  return new Promise((resolve, reject) => {
+    movieDb.movieInfo({id: movie_id}, (err, res) => {
+      if (err) {
+        console.log(err)
+        reject(err)
+      }
+      console.log(`${res.original_title}, ${res.release_date.slice(0,4)}, ${res.overview}`)
+      resolve(res)
+    })
+  })
 }
 
 module.exports = {
-  allGenres: allGenres,
-  getMovie: getMovie,
-  getMovieGenres: getMovieGenres
+  searchMovies,
+  addRandom,
+  addTitle
+  // allGenres: allGenres,
+  // getMovie: getMovie,
+  // getMovieGenres: getMovieGenres
 }
-
--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,31 @@ router.get('/', function (req, res) {
 })
 
 router.get('/addmovie', function (req, res) {
-  db.allGenres(req.params.id, req.app.get('connection'))
-    .then((genres) => console.log(genres))
-    // .then((genres) => res.render('addmovie', genres))
+  // db.allGenres(req.params.id, req.app.get('connection'))
+  //   .then((genres) => console.log(genres))
+  //   // .then((genres) => res.render('addmovie', genres))
+  res.render("addmovie")
+})
 
+router.post('/addmovie', (req, res) => {
+  if (req.body.random) {
+    db.addRandom()
+    res.render("addmovie")
+  } else if (req.body.search) {
+    db.searchMovies(req.body.search)
+      .then((page) => {
+          page["results"].forEach((movie) => {
+          movie["release_date"] = movie["release_date"].slice(0, 4)
+        })
+        res.render("addmovie", page)
+      })
+      .catch(console.log)
+  } else if (req.body.addtitle) {
+    db.addTitle(req.body.addtitle, req.app.get('connection'))
+      .then(() => {
+        res.render("addmovie")
+      })
+  }
 })
 
 router.get('/movie/:id', function (req, res) {
