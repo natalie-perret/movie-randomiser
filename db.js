852931dd930e5bb426121571643b699138f063cf
natalie-perret <nperret@gmail.com>
2017-05-12 15:32:52 +1200

    Update db.js

--- db.js
+++ db.js
@@ -146,7 +146,7 @@ module.exports = {
   allMovies: allMovies,
   genreId: genreId,
   getGenreMovies: getGenreMovies,
-  randomise: randomise
+  randomise: randomise,
   searchMovies,
   addRandom,
   addTitle,
